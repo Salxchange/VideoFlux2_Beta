ab22b8e35f3c824be5790830a926fbc4befcf2e5
Salxchange <138751618+Salxchange@users.noreply.github.com>
2024-03-06 21:53:34 +0100

    Update start.py

--- bot/start.py
+++ bot/start.py
@@ -717,8 +717,8 @@ async def _speed_test(event):
 async def _startmsg(event):
     text = f"Hi {get_mention(event)}, I Am Alive."
     await event.reply(text, buttons=[
-    [Button.url('⭐ Bot By 𝚂𝚊𝚑𝚒𝚕 ⭐', 'https://t.me/nik66')],
-    [Button.url('❤ Join Channel ❤', 'https://t.me/nik66x')]
+    [Button.url('⚡ 𝖴𝗉𝖽𝖺𝗍𝖾𝗌', 'https://t.me/Rokubotz')],
+    [Button.url('⚡ 𝖲𝗎𝗉𝗉𝗈𝗋𝗍', 'https://t.me/Team_Roku')]
 ])
     return
 
